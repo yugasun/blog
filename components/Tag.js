f68370a4a876be6f7a82055d246308c3a712e445
yugasun <yuga_sun@163.com>
2023-05-21 16:05:42 +0800

    fix: link style

--- components/Tag.js
+++ components/Tag.js
@@ -3,13 +3,12 @@ import kebabCase from '@/lib/utils/kebabCase'
 
 const Tag = ({ text }) => {
   return (
-    <Link
+    <a
       href={`/tags/${kebabCase(text)}`}
       className="mr-3 text-sm font-medium text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-      legacyBehavior
     >
       {text.split(' ').join('-')}
-    </Link>
+    </a>
   )
 }
 
