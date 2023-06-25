48a9947c8ce3b613ae37c94d5a71f4e8e3401822
yugasun <yuga_sun@163.com>
2023-06-25 19:03:47 +0800

    fix: page route rewrite

--- middleware.js
+++ middleware.js
@@ -1,12 +1,13 @@
 import { rewrite, next } from '@vercel/edge'
 
 export default function middleware(request) {
+  const url = new URL(request.url)
   // if request url contain .html, remove it
-  if (request.url.includes('.html')) {
-    return rewrite(new URL(request.url.replace('.html', ''), request.url))
+  if (url.pathname.includes('.html')) {
+    return rewrite(new URL(url.pathname.replace('.html', ''), request.url))
   }
-  if (/page\/(\d+)/g.test(request.url) && !request.url.includes('/post')) {
-    return rewrite(new URL(`/post${request.url}`, request.url))
+  if (/page\/(\d+)/g.test(url.pathname) && !url.pathname.includes('/post')) {
+    return rewrite(new URL(`/post${url.pathname}`, request.url))
   }
   return next()
 }
