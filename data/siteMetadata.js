5212621b819e38711ad66d440cff9fbddd7e61b2
yugasun <yuga_sun@163.com>
2023-06-25 18:12:41 +0800

    fix: add page rewrite

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,8 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. blog.yugasun.com
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-ZWCY7V55PC', // e.g. UA-000000-2 or G-XXXXXXX
+    // googleAnalyticsId: 'G-ZWCY7V55PC', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-FYDJ6H7WRJ', // e.g. UA-000000-2 or G-XXXXXXX
     baiduAnalyticsId: '28d4aea1680535e760d13d954b54eddc', // e.g. 28d4xxx
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
     // google adsense
--- middleware.js
+++ middleware.js
@@ -5,5 +5,8 @@ export default function middleware(request) {
   if (request.url.includes('.html')) {
     return rewrite(new URL(request.url.replace('.html', ''), request.url))
   }
+  if (/page\/(\d+)/g.test(request.url) && !request.url.includes('/post')) {
+    return rewrite(new URL(`/post${request.url}`, request.url))
+  }
   return next()
 }
